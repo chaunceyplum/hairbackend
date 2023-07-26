2056332132255fac6bffd8d8499b8e8c03e79964
Chauncey <Chaunceyplum@gmail.com>
2023-07-25 23:21:51 -0400

    fixed prisma issue

--- app.py
+++ app.py
@@ -32,6 +32,10 @@ app.register_blueprint(appointment_blueprint, url_prefix='/appointment')
 app.register_blueprint(transaction_blueprint, url_prefix='/transaction')
 app.register_blueprint(login_blueprint, url_prefix='/login')
 
+@app.route('/')
+def index():
+    return {'status':'up'}
+
 def main() -> None:
   db = Prisma()
   db.connect()
